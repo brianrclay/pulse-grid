c9f0b09eac32b9ee1e2e59cca9be1bca0b79f8eb
Brian Clay <brian.clay@mx.com>
2020-09-28 22:30:31 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+$('.toggle-drawer').click(function(){
+    $('.drawer').toggleClass('open');
+})
+
+$('.close-drawer, .close').click(function(){
+    $('.drawer').removeClass('open');
+})
+
+
+$('.show-modal').click(function(){
+    $('.modal, .modal-overlay').addClass('open');
+})
+
+$('.modal-overlay, .modal .close, .modal .close-modal').click(function(){
+    $('.modal, .modal-overlay').removeClass('open');
+});
+
+$('.mobile-menu').click(function(){
+    $('.col.left').toggleClass('show')
+})
